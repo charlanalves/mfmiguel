1020a3bc72e7a4a762935f5280772342bc47ae7d
charlanalves <charlan.job@gmail.com>
2017-03-26 15:31:51 -0300

    layout categoria blog e item

--- templates/hoxa/html/com_k2/templates/blog/item.php
+++ templates/hoxa/html/com_k2/templates/blog/item.php
@@ -29,7 +29,53 @@ $postLink = $extraFields[1]->value;
     <div class="blog_postcontent">
       <!-- Post type -->
       <?php if($postType == '1') : ?>
-        <div class="image_frame"><a href="<?php echo $this->item->link; ?>"><img src="<?php echo $this->item->imageXLarge;//JURI::root(true).$postLink;?>" alt="<?php echo $this->item->title;?>" /></a></div>
+        <div class="image_frame"><a href="<?php echo $this->item->link; ?>"><img src="<?php echo $this->item->imageXLarge;//JURI::root(true).$postLink;?>" alt="<?php echo $this->item->title;?>" /></a>
+		
+		 <ul class="post_meta_links area_item">
+        <!-- post author -->
+          <li><a href="#" class="date"><?php echo JHTML::_('date', $this->item->created, 'd F Y'); ?></a></li>
+        <?php if($this->item->params->get('itemAuthor')): ?>
+          <li class="post_by"><i><?php echo JText::_('TPL_HOXA_POSTED_BY_TEXT');?></i> 
+          <?php if(isset($this->item->author->link) && $this->item->author->link): ?>
+            <a href="<?php echo $this->item->author->link; ?>"><?php echo $this->item->author->name; ?></a>
+          <?php else: ?>
+            <?php echo $this->item->author->name; ?>
+          <?php endif; ?>
+          </li>
+        <?php endif; ?>
+      
+      <!-- post category -->
+      <?php if($this->item->params->get('itemCategory')): ?>
+        <li class="post_categoty"><i><?php echo JText::_('TPL_HOXA_POSTED_IN_TEXT');?></i> <a href="<?php echo $this->item->category->link; ?>"><?php echo $this->item->category->name;?></a></li>
+      <?php endif; ?>
+            
+            <!-- post comment -->
+            <?php if($this->item->params->get('itemCommentsAnchor') && ( ($this->item->params->get('comments') == '2' && !$this->user->guest) || ($this->item->params->get('comments') == '1')) ): ?>
+      
+        <li class="post_comments"><i><?php echo JText::_('TPL_HOXA_POSTED_NOTE_TEXT');?></i>
+
+        <?php if(!empty($this->item->event->K2CommentsCounter)): ?>
+          <!-- K2 Plugins: K2CommentsCounter -->
+          <?php echo $this->item->event->K2CommentsCounter; ?>
+
+        <?php else: ?>
+          <?php if($this->item->numOfComments > 0): ?>
+          <a href="<?php echo $this->item->link; ?>#itemCommentsAnchor">
+            <?php echo $this->item->numOfComments; ?> <?php echo ($this->item->numOfComments>1) ? JText::_('K2_COMMENTS') : JText::_('K2_COMMENT'); ?>
+            </a>
+          <?php else: ?>
+          <a href="<?php echo $this->item->link; ?>#itemCommentsAnchor">
+            <?php echo JText::_('K2_BE_THE_FIRST_TO_COMMENT'); ?>
+          </a>
+          <?php endif; ?>
+        <?php endif; ?>
+        </li>
+      <?php endif; ?>
+
+            
+        </ul>
+		
+		</div>
     <?php elseif($postType == '4') : ?>
       <div class="image_frame">
         <ul class="slides">
@@ -42,6 +88,8 @@ $postLink = $extraFields[1]->value;
         </li>
     <?php endif; endforeach; ?>
         </ul>
+		
+		
       </div>
     <?php elseif($postType == '2') : 
       $id = array();
@@ -86,53 +134,8 @@ $postLink = $extraFields[1]->value;
         <!-- Post title -->
     <h3><a href="<?php echo $this->item->link; ?>"><?php echo $this->item->title;?></a></h3>
 
-        <ul class="post_meta_links">
-        <!-- post author -->
-          <li><a href="#" class="date"><?php echo JHTML::_('date', $this->item->created, 'd F Y'); ?></a></li>
-        <?php if($this->item->params->get('itemAuthor')): ?>
-          <li class="post_by"><i><?php echo JText::_('TPL_HOXA_POSTED_BY_TEXT');?></i> 
-          <?php if(isset($this->item->author->link) && $this->item->author->link): ?>
-            <a href="<?php echo $this->item->author->link; ?>"><?php echo $this->item->author->name; ?></a>
-          <?php else: ?>
-            <?php echo $this->item->author->name; ?>
-          <?php endif; ?>
-          </li>
-        <?php endif; ?>
-      
-      <!-- post category -->
-      <?php if($this->item->params->get('itemCategory')): ?>
-        <li class="post_categoty"><i><?php echo JText::_('TPL_HOXA_POSTED_IN_TEXT');?></i> <a href="<?php echo $this->item->category->link; ?>"><?php echo $this->item->category->name;?></a></li>
-      <?php endif; ?>
-            
-            <!-- post comment -->
-            <?php if($this->item->params->get('itemCommentsAnchor') && ( ($this->item->params->get('comments') == '2' && !$this->user->guest) || ($this->item->params->get('comments') == '1')) ): ?>
-      
-        <li class="post_comments"><i><?php echo JText::_('TPL_HOXA_POSTED_NOTE_TEXT');?></i>
-
-        <?php if(!empty($this->item->event->K2CommentsCounter)): ?>
-          <!-- K2 Plugins: K2CommentsCounter -->
-          <?php echo $this->item->event->K2CommentsCounter; ?>
-
-        <?php else: ?>
-          <?php if($this->item->numOfComments > 0): ?>
-          <a href="<?php echo $this->item->link; ?>#itemCommentsAnchor">
-            <?php echo $this->item->numOfComments; ?> <?php echo ($this->item->numOfComments>1) ? JText::_('K2_COMMENTS') : JText::_('K2_COMMENT'); ?>
-            </a>
-          <?php else: ?>
-          <a href="<?php echo $this->item->link; ?>#itemCommentsAnchor">
-            <?php echo JText::_('K2_BE_THE_FIRST_TO_COMMENT'); ?>
-          </a>
-          <?php endif; ?>
-        <?php endif; ?>
-        </li>
-      <?php endif; ?>
-
-            
-        </ul>
-        <div class="clearfix"></div>
-        <div class="margin_top1"></div>
-
-      <?php if(!empty($this->item->fulltext)): ?>
+       
+		<?php if(!empty($this->item->fulltext)): ?>
         <?php if($this->item->params->get('itemIntroText')): ?>
         <!-- Item introtext -->
           <?php echo $this->item->introtext; ?>
@@ -147,6 +150,10 @@ $postLink = $extraFields[1]->value;
             <?php echo $this->item->introtext; ?>
           <?php endif; ?>
       <?php endif; ?>
+        <div class="clearfix"></div>
+        <div class="margin_top1"></div>
+
+      
 
 
     </div>
